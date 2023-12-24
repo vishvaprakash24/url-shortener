42aa0f6a88b0391c204973c3351c9fe54d4d300a
Vishva <vishva@beanbyte.dev>
2023-12-25 00:56:36 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const URL = require("./models/url");
+const app = express();
+const PORT = 8001;
+
+const { connectToMongoDB } = require("./connect");
+const urlRoute = require("./routes/url")
+
+
+connectToMongoDB('mongodb://localhost:27017/short-url')
+.then(()=> console.log("Mongodb connected"))
+
+app.use(express.json());
+
+app.use("/url", urlRoute);
+
+app.get('/:shortId', async (req, res) => {
+    const shortId = req.params.shortId;
+    const entry = await URL.findOneAndUpdate({
+        shortId,
+    },{$push: {
+        visitHistory: {
+            timestamp: Date.now(),
+        },
+    },
+}
+    );
+    res.redirect(entry.redirectURL)
+})
+
+app.listen(PORT, () => console.log(`Server started at port: ${PORT}`))
